c6005958de8664fe81ffc6a7f4bc8750b5634381
andydbui <andy1000ek@hotmail.com>
2024-01-31 00:35:26 -0500

    first commit

---
+++ contents/mods/DIY Vehicle Parts ATA Mustang/media/lua/server/items/DIY_VehicleParts_Fjord_Mustard.lua
@@ -0,0 +1,62 @@
+require 'Items/SuburbsDistributions'
+require 'Items/ProceduralDistributions'
+require 'Vehicles/VehicleDistributions'
+
+-- Vehicle Distributions
+table.insert(VehicleDistributions["GloveBox"].items, "DIY_VehicleParts_Fjord_MustardMag");
+table.insert(VehicleDistributions["GloveBox"].items, 0.05);
+
+-- Basic
+table.insert(ProceduralDistributions["list"]["BedroomDresser"].items, "DIY_VehicleParts_Fjord_MustardMag");
+table.insert(ProceduralDistributions["list"]["BedroomDresser"].items, 0.2);
+
+table.insert(ProceduralDistributions["list"]["PlankStashMagazine"].items, "DIY_VehicleParts_Fjord_MustardMag");
+table.insert(ProceduralDistributions["list"]["PlankStashMagazine"].items, 2.0);
+
+table.insert(ProceduralDistributions["list"]["OfficeDeskHome"].items, "DIY_VehicleParts_Fjord_MustardMag");
+table.insert(ProceduralDistributions["list"]["OfficeDeskHome"].items, 0.05);
+
+table.insert(ProceduralDistributions["list"]["SchoolLockers"].items, "DIY_VehicleParts_Fjord_MustardMag");
+table.insert(ProceduralDistributions["list"]["SchoolLockers"].items, 0.01);
+
+table.insert(ProceduralDistributions["list"]["Locker"].items, "DIY_VehicleParts_Fjord_MustardMag");
+table.insert(ProceduralDistributions["list"]["Locker"].items, 0.025);
+
+table.insert(ProceduralDistributions["list"]["CrateMagazines"].items, "DIY_VehicleParts_Fjord_MustardMag");
+table.insert(ProceduralDistributions["list"]["CrateMagazines"].items, 0.2);
+
+table.insert(ProceduralDistributions["list"]["DeskGeneric"].items, "DIY_VehicleParts_Fjord_MustardMag");
+table.insert(ProceduralDistributions["list"]["DeskGeneric"].items, 0.2);
+
+table.insert(ProceduralDistributions["list"]["ClassroomDesk"].items, "DIY_VehicleParts_Fjord_MustardMag");
+table.insert(ProceduralDistributions["list"]["ClassroomDesk"].items, 0.025);
+
+table.insert(ProceduralDistributions["list"]["MagazineRackMixed"].items, "DIY_VehicleParts_Fjord_MustardMag");
+table.insert(ProceduralDistributions["list"]["MagazineRackMixed"].items, 0.2);
+
+table.insert(ProceduralDistributions["list"]["PostOfficeMagazines"].items, "DIY_VehicleParts_Fjord_MustardMag");
+table.insert(ProceduralDistributions["list"]["PostOfficeMagazines"].items, 0.7);
+
+table.insert(ProceduralDistributions["list"]["ToolStoreBooks"].items, "DIY_VehicleParts_Fjord_MustardMag");
+table.insert(ProceduralDistributions["list"]["ToolStoreBooks"].items, 2.5);
+
+table.insert(SuburbsDistributions["all"]["sidetable"].items, "DIY_VehicleParts_Fjord_MustardMag");
+table.insert(SuburbsDistributions["all"]["sidetable"].items, 0.1);
+
+table.insert(SuburbsDistributions["all"]["postbox"].items, "DIY_VehicleParts_Fjord_MustardMag");
+table.insert(SuburbsDistributions["all"]["postbox"].items, 0.01);
+
+table.insert(ProceduralDistributions["list"]["BookstoreBooks"].items, "DIY_VehicleParts_Fjord_MustardMag");
+table.insert(ProceduralDistributions["list"]["BookstoreBooks"].items, 1.5);
+
+table.insert(ProceduralDistributions["list"]["LibraryBooks"].items, "DIY_VehicleParts_Fjord_MustardMag");
+table.insert(ProceduralDistributions["list"]["LibraryBooks"].items, 1.5);
+
+table.insert(ProceduralDistributions["list"]["GarageMetalwork"].items, "DIY_VehicleParts_Fjord_MustardMag");
+table.insert(ProceduralDistributions["list"]["GarageMetalwork"].items, 2.0);
+
+table.insert(ProceduralDistributions["list"]["CrateMagazines"].items, "DIY_VehicleParts_Fjord_MustardMag");
+table.insert(ProceduralDistributions["list"]["CrateMagazines"].items, 1.5);
+
+table.insert(SuburbsDistributions["all"]["bin"].items, "DIY_VehicleParts_Fjord_MustardMag");
+table.insert(SuburbsDistributions["all"]["bin"].items, 0.05);
